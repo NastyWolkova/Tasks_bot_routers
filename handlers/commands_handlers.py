98c58aa7834d1ed63b49d3bb89c272e3351d1ea6
NastyWolkova <132331458+NastyWolkova@users.noreply.github.com>
2023-08-01 11:07:16 +0300

    Create bot with routers(Tasks_bot v.2)

--- handlers/commands_handlers.py
+++ handlers/commands_handlers.py
@@ -1,40 +1,38 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
-from lexicon.lexicon import users, tasks
+from lexicon.lexicon import users, tasks, answers
 import datetime
 
 router: Router = Router()
 
 @router.message(Command(commands=['start']))
 async def command_start(message: Message):
-    if message.from_user.id in users and (users[message.from_user.id]['current_task'] == 0 or datetime.datetime.now() >= users[message.from_user.id]['finish_time']):
-        if users[message.from_user.id]['current_task'] != 0:
-            users[message.from_user.id]['current_task'] = 0
-        await message.answer(f'Вы уже проходили испытания. \nВаши баллы: {users[message.from_user.id]["total_score"]}')
-    elif message.from_user.id in users and users[message.from_user.id]['current_task'] != 0:
-        await message.answer(f'Вы проходите испытания. '
-                             f'\nДля получения следующего задания отправьте ответ.')
-    else:
+    if message.from_user.id not in users:
+        #запуск игры
         dt1 = datetime.datetime.today()
         dt2 = dt1 + datetime.timedelta(minutes=3)
-        print(dt1, dt2)
         users[message.from_user.id] = {'current_task': 0,
-                                        'total_score': 0,
-                                        'start_time': dt1,
-                                        'finish_time': dt2,
-                                        1: 0,
-                                        2: 0,
-                                        3: 0}
+                                       'total_score': 0,
+                                       'start_time': dt1,
+                                       'finish_time': dt2,
+                                       1: 0,
+                                       2: 0,
+                                       3: 0}
         users[message.from_user.id]['current_task'] = 1
-        await message.answer(f'Тестирование состоит из 3 задач, \nкоторые надо решить за 3 минуты.'
-                             f'\nЗадача №1: {tasks[1][0]}')
+        print(users)
+        await message.answer(f"{answers['start1']} \n{tasks[1][0]}")
+    else:
+        if users[message.from_user.id]['current_task'] == 0:
+            await message.answer(f"{answers['game_over']} {users[message.from_user.id]['total_score']}")
+        else:
+            if datetime.datetime.now() >= users[message.from_user.id]['finish_time']:
+                users[message.from_user.id]['current_task'] = 0
+                await message.answer(f"{answers['time_over']} {users[message.from_user.id]['total_score']}")
+            else:
+                await message.answer(f"{answers['into_game']}")
+
 
 @router.message(Command(commands=['help']))
-async def command_help(message:Message):
-    await message.answer(f'Пройти испытание можно только один раз.'
-                         f'\nВремя тестирования - 3 минуты.'
-                         f'\nПолучить первое задание можно командой /start.'
-                         f'\nОтвет должен быть числом.'
-                         f'\nВсе задачи решаются последовательно - '
-                         f'\nследующее задание отправится после получения от вас ответа на предыдущее.')
+async def command_help(message: Message):
+    await message.answer(f"{answers['help']}")
